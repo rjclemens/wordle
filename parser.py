e5e0ab8d37ce2ac90deb97def78492995df5d63e
rjclemens <rjclemens518@gmail.com>
2022-02-28 01:39:40 -0600

    interactive wordle

---
+++ parser.py
@@ -0,0 +1,15 @@
+import statistics as stats
+
+with open('exp_stg1_raw.txt') as file:
+    data = file.read().split('\n')
+
+data = [(e.split(':')[0], float(e.split(':')[1][1:])) for e in data]
+
+data = list(sorted(data, key=lambda x: x[1], reverse=True))
+
+avg = sum([x[1] for x in data])/len(data)
+med = stats.median([x[1] for x in data])
+
+print(f'Average:\t{avg}\t\tMedian:\t{med}')
+for x in data:
+    print(f'{x[0]}:\t{x[1]}')
--- wordle.py
+++ wordle.py
@@ -1,17 +1,27 @@
 import math
 import itertools as it
+import random
+from termcolor import colored, cprint
+
+with open('words.txt') as file:
+    WORDS = file.read().split(',')
+
+with open('possible_words.txt') as file:
+    P_WORDS = file.read().split('\n')
 
 GREY = 0
 TAN = 1
 GREEN = 2
 
-with open('words.txt') as word_file:
-    DATA = word_file.read().split(',')
+COLORS = ['on_white', 'on_yellow', 'on_green']
 
-DATA_SIZE = len(DATA)
-print(DATA_SIZE)
+WORDS_SIZE = len(WORDS)
+P_WORDS_SIZE = len(P_WORDS)
 
 vals = list(it.product([GREY, TAN, GREEN], repeat=5))
+
+RESPS = []
+GUESSES = []
 
 
 def find_words(conditions, guess, data):
@@ -20,44 +30,93 @@ def find_words(conditions, guess, data):
         if v == GREY:
             data = [x for x in data if ch not in x]
         elif v == TAN:
-            data = [x for x in data if ch in x]
+            data = [x for x in data if (ch in x and ch != x[i])]
         elif v == GREEN:
             data = [x for x in data if ch == x[i]]
-
-    # print(conditions, guess, len(data))
-    # print("-----------")
     return data
 
 
-def info(data):
-    try:
-        return math.log2(DATA_SIZE / len(data))
-    except:
-        return 0
+def info(prob):
+    return math.log2(1/prob) if prob > 0 else 0
 
 
 def exp(word, data):
     exp_val = 0
     for val in vals:
         match = find_words(val, word, data)
-        exp_val += info(match) * (len(match) / DATA_SIZE)
+        prob = len(match) / WORDS_SIZE
+        exp_val += info(prob) * prob
 
-    print(f'{word}:\t{exp_val}')
+    # print(f'{word}:\t{exp_val}')
     return exp_val
 
 
 def find_st_word(data):
-    best_words = []
-    # best_words = [(word, exp(word, data)) for word in data]
-    for word in data:
-        best_words.append((word, exp(word, data)))
+    best_words = [(word, exp(word, data)) for word in data]
+    return list(sorted(best_words, key=lambda x: x[1], reverse=True))
 
-    return list(sorted(best_words, key=lambda x: x[1]))
+
+def print_colored(resp, guess):
+    for i, ch in enumerate(guess):
+        cprint(ch, 'grey', COLORS[resp[i]], end='')
+
+
+def play(resp, guess, data):
+    GUESSES.append(guess)
+    RESPS.append(resp)
+    match = find_words(resp, guess, data)
+
+    for i in range(len(GUESSES)):
+        print_colored(RESPS[i], GUESSES[i])
+        print()
+
+    print(f'\nInformation:\t{info(len(match)/WORDS_SIZE)}')
+    print(f'Entropy:\t{exp(guess, data)}')
+    print(f'Uncertainty:\t{math.log2(len(match))}')
+    print(len(match), end=': ')
+
+    nexts = 5 if len(match) > 5 else len(match)-1
+    for i in range(nexts):
+        print(match[i], end=', ')
+    print(match[nexts])
+
+    return match
+
+
+def eval(key, guess):
+    resp = []
+    for i, ch in enumerate(guess):
+        color = TAN
+        if key[i] == ch:
+            color = GREEN
+        if ch not in key:
+            color = GREY
+        resp.append(color)
+
+    return resp
+
+
+def build_puzzle(data):
+    RESPS = []
+    GUESSES = []
+    KEY = P_WORDS[random.randrange(0, P_WORDS_SIZE)]
+    poss, guess = WORDS, ""
+    while guess != KEY:
+        guess = input('\nWord Guess: ')
+        resp = eval(KEY, guess)
+        poss = play(resp, guess, poss)
+    print_colored(eval(KEY, guess), guess)
 
 
 def main():
-    for word in find_st_word(DATA):
-        print(f'{word[0]}:\t{word[1]}')
+    # for word in find_st_word(WORDS):
+    #     print(f'{word[0]}:\t{word[1]}')
+    # find_st_word(WORDS)
+    # word = 'tares'
+    # print(word + ":\t", exp(word, WORDS))
+    while True:
+        build_puzzle(WORDS)
+        print('\n---------------------')
 
 
 if __name__ == '__main__':
